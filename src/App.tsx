a7ace9f44bbebb5ee1c9b6a18d3e6660482ec290
abhinav <abhinav.malkoochi@gmail.com>
2023-03-07 21:54:19 -0600

    dropdown testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,34 @@
 import React, { Component } from "react";
+/*
+TODO: 
+1. populate drop-down with all books from API
+2. Collect data with text, reviews, and ratings
+3. NLP to analyze input data and provide suggestions
+4. Compare input book with database to suggest similar book
+
+
+*/
+
+import DropdownButton from "react-bootstrap/DropdownButton";
+import Dropdown from "react-dropdown";
+import "react-dropdown/style.css";
 
 export default class App extends Component {
   render() {
-    return <div>App</div>;
+    const options = ["one", "two", "three"];
+
+    return (
+      <div>
+        <main>
+          <section>
+            <Dropdown
+              options={options}
+              value="B"
+              placeholder="Select an option"
+            />
+          </section>
+        </main>
+      </div>
+    );
   }
 }
